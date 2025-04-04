1327105070e186a3d8ed5fd6879e67552fd1fc03
KieranDeguara20 <deguarakieran26@gmail.com>
2025-04-04 16:47:30 +0200

    Make use of Constructor Injection and Method Injection and saves in database

---
+++ Presentation/Controllers/PollController.cs
@@ -0,0 +1,35 @@
+﻿using DataAccess.Repositories;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    public class PollController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreatePoll()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreatePoll(Poll poll, [FromServices] PollRepository pollRepository)
+        {
+            if (ModelState.IsValid)
+            {
+                poll.DateCreated = DateTime.Now;
+                poll.Option1VotesCount = 0;
+                poll.Option2VotesCount = 0;
+                poll.Option3VotesCount = 0;
+
+                pollRepository.CreatePoll(poll);
+                return View();
+            }
+            return View(poll);
+        }
+    }
+}
--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,7 +1,7 @@
 using DataAccess.DataContext;
+using DataAccess.Repositories;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.SqlServer.Server;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +14,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<PollDbContext>();
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<PollRepository>();
 
 var app = builder.Build();
 
